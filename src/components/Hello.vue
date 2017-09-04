06514b8650ad88fe3a26f9210b3fc46d033ee6f3
Erik Erikson <usingpond@me.com>
2017-09-04 16:01:08 -0400

    initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1 class="heading">Hi, this is hello world.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+};
+</script>
+
+<style scoped lang="scss">
+.heading {
+  color: red;
+}
+</style>
